6e15d5d6f285c3bf20a03ce2131f444fead1ee8a
Lukas Harbarth <lukas.harbarth@sap.com>
2023-03-27 15:04:55 +0200

    use `zod` instead of `yup`, cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useCallback, useState } from "react";
+import { useState } from "react";
 import {
   Button,
   CheckBox,
   DatePicker,
   Form,
   FormItem,
+  FormGroup,
   Input,
   InputType,
   Label,
@@ -14,54 +15,25 @@ import {
   Select,
   ThemeProvider,
 } from "@ui5/webcomponents-react";
-import * as Yup from "yup";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 //For the submits property of the button to have effect, you must add the following import
 import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";
 
-const LoginSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Invalid email address format")
-    .required("Email is required"),
-  password: Yup.string()
-    .min(5, "Password must be 5 characters at minimum")
-    .required("Password is required"),
-  terms: Yup.boolean().oneOf([true], "The terms of service must be accepted"),
+const zodSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email address format" }),
+  password: z.string().min(5, "Password must be 5 characters at minimum"),
+  terms: z.literal(true, {
+    errorMap: () => ({ message: "The terms of service must be accepted" }),
+  }),
 });
 
-const useYupValidationResolver = (validationSchema) =>
-  useCallback(
-    async (data) => {
-      try {
-        const values = await validationSchema.validate(data, {
-          abortEarly: false,
-        });
-
-        return {
-          values,
-          errors: {},
-        };
-      } catch (errors) {
-        return {
-          values: {},
-          errors: errors.inner.reduce(
-            (allErrors, currentError) => ({
-              ...allErrors,
-              [currentError.path]: {
-                type: currentError.type ?? "validation",
-                message: currentError.message,
-              },
-            }),
-            {}
-          ),
-        };
-      }
-    },
-    [validationSchema]
-  );
-
 export default function LoginForm() {
-  const resolver = useYupValidationResolver(LoginSchema);
+  const resolver = zodResolver(zodSchema);
   const {
     register,
     handleSubmit,
@@ -81,105 +53,116 @@ export default function LoginForm() {
     },
   });
   const [invalidEntries, setInvalidEntries] = useState({});
+  // example for registering only part of the register properties
+  const { ref: cbRef, name: cbName } = register("terms");
   // if you need to fully control the state, you can use `getValues` (see `CheckBox`)
   const values = getValues();
 
   const onSubmit = (data) => {
-    alert(`Submitted fields:\n${JSON.stringify(data, null, 2)}`);
     if (Object.keys(invalidEntries).length) {
       Object.entries(invalidEntries).filter(([name, invalid]) => {
-        // use this to catch internal (not validated by yup) validation of components
+        // use this to catch internal (not validated by zod) validation of components
         if (invalid) {
           setError(name, { type: "internal" }, { shouldFocus: true });
         }
       });
+    } else {
+      alert(`Submitted fields:\n${JSON.stringify(data, null, 2)}`);
     }
   };
 
   return (
     <ThemeProvider>
       <div style={{ maxWidth: "800px" }}>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <FormItem
-            label={
-              <Label required showColon>
-                Email
-              </Label>
-            }
-          >
-            <Input
-              type={InputType.Email}
-              {...register("email", { required: true })}
-              onInput={(e) => {
-                setValue("email", e.target.value);
-              }}
-              valueState={errors.email ? "Error" : "None"}
-              valueStateMessage={<div>{errors.email?.message}</div>}
-            />
-          </FormItem>
-          <FormItem
-            label={
-              <Label required showColon>
-                Password
-              </Label>
-            }
-          >
-            <Input
-              type={InputType.Password}
-              {...register("password", { required: true })}
-              onInput={(e) => {
-                setValue("password", e.target.value);
-              }}
-              valueState={errors.password ? "Error" : "None"}
-              valueStateMessage={<div>{errors.password?.message}</div>}
-            />
-          </FormItem>
-          <FormItem label="Country">
-            <Select
-              {...register("country")}
-              onChange={(e) => {
-                setValue("country", e.detail.selectedOption.textContent);
-              }}
+        <Form
+          labelSpanS={4}
+          labelSpanM={4}
+          labelSpanL={4}
+          labelSpanXL={4}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <FormGroup>
+            <FormItem
+              label={
+                <Label required showColon>
+                  Email
+                </Label>
+              }
             >
-              <Option>Germany</Option>
-              <Option>France</Option>
-              <Option>Italy</Option>
-            </Select>
-          </FormItem>
-          <FormItem label={<Label showColon>Date of Birth</Label>}>
-            <DatePicker
-              {...register("dob")}
-              onChange={(e) => {
-                if (e.detail.valid) {
-                  setValue("dob", e.detail.value);
-                  // internal validation
-                  setInvalidEntries((prev) => ({ ...prev, dob: false }));
-                } else {
-                  // internal validation
-                  setInvalidEntries((prev) => ({ ...prev, dob: true }));
-                }
-              }}
-            />
-          </FormItem>
-          <FormItem label="Payment methods">
-            <MultiComboBox
-              {...register("payment", {
-                onChange: undefined,
-                onBlur:
-                  undefined /*  disable `onChange` and `onBlur` to prevent unwanted submission of strings*/,
-              })}
-              onSelectionChange={(e) => {
-                setValue(
-                  "payment",
-                  e.detail.items.map((item) => item.getAttribute("text"))
-                );
-              }}
+              <Input
+                type={InputType.Email}
+                {...register("email", { required: true })}
+                onInput={(e) => {
+                  setValue("email", e.target.value);
+                }}
+                valueState={errors.email ? "Error" : "None"}
+                valueStateMessage={<div>{errors.email?.message}</div>}
+              />
+            </FormItem>
+            <FormItem
+              label={
+                <Label required showColon>
+                  Password
+                </Label>
+              }
             >
-              <MultiComboBoxItem text="Credit card" />
-              <MultiComboBoxItem text="PayPal" />
-              <MultiComboBoxItem text="Bank transfer" />
-            </MultiComboBox>
-          </FormItem>
+              <Input
+                type={InputType.Password}
+                {...register("password", { required: true })}
+                onInput={(e) => {
+                  setValue("password", e.target.value);
+                }}
+                valueState={errors.password ? "Error" : "None"}
+                valueStateMessage={<div>{errors.password?.message}</div>}
+              />
+            </FormItem>
+            <FormItem label="Country">
+              <Select
+                {...register("country")}
+                onChange={(e) => {
+                  setValue("country", e.detail.selectedOption.textContent);
+                }}
+              >
+                <Option>Germany</Option>
+                <Option>France</Option>
+                <Option>Italy</Option>
+              </Select>
+            </FormItem>
+            <FormItem label={<Label showColon>Date of Birth</Label>}>
+              <DatePicker
+                {...register("dob")}
+                onChange={(e) => {
+                  if (e.detail.valid) {
+                    setValue("dob", e.detail.value);
+                    // internal validation
+                    setInvalidEntries((prev) => ({ ...prev, dob: false }));
+                  } else {
+                    // internal validation
+                    setInvalidEntries((prev) => ({ ...prev, dob: true }));
+                  }
+                }}
+              />
+            </FormItem>
+            <FormItem label="Payment methods">
+              <MultiComboBox
+                {...register("payment", {
+                  onChange: undefined,
+                  onBlur:
+                    undefined /*  disable `onChange` and `onBlur` to prevent unwanted submission of strings*/,
+                })}
+                onSelectionChange={(e) => {
+                  setValue(
+                    "payment",
+                    e.detail.items.map((item) => item.getAttribute("text"))
+                  );
+                }}
+              >
+                <MultiComboBoxItem text="Credit card" />
+                <MultiComboBoxItem text="PayPal" />
+                <MultiComboBoxItem text="Bank transfer" />
+              </MultiComboBox>
+            </FormItem>
+          </FormGroup>
           <FormItem
             label={
               <Label required showColon>
@@ -188,7 +171,8 @@ export default function LoginForm() {
             }
           >
             <CheckBox
-              {...register("terms")}
+              ref={cbRef}
+              name={cbName}
               onChange={(e) => {
                 setValue("terms", e.target.checked);
               }}
